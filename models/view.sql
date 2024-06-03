209943d7d13ef8e90a0059693bee75957907d2e2
Wafi-Afdi <wafiafdi@gmail.com>
2024-06-03 22:55:25 +0700

    update : views baru

--- models/view.sql
+++ models/view.sql
@@ -10,9 +10,9 @@ SELECT buku.id AS id_buku,
        relasiCabangBuku.kuantitas AS kuantitas,
        relasiCabangBuku.id_cabang AS kode_cabang
 FROM public.buku buku
-JOIN public.relasi_buku_promo relasiBukuPromo ON buku.id = relasiBukuPromo.id_buku
-JOIN public.promo eventPromo ON eventPromo.id = relasiBukuPromo.id_promo
-JOIN public.relasi_cabang_buku relasiCabangBuku ON relasiCabangBuku.id_buku = buku.id;
+LEFT JOIN public.relasi_buku_promo relasiBukuPromo ON buku.id = relasiBukuPromo.id_buku
+LEFT JOIN public.promo eventPromo ON eventPromo.id = relasiBukuPromo.id_promo
+LEFT JOIN public.relasi_cabang_buku relasiCabangBuku ON relasiCabangBuku.id_buku = buku.id;
 
 CREATE VIEW public.buku_dengan_cabang AS
 SELECT buku.id AS id_buku,
@@ -26,8 +26,15 @@ SELECT buku.id AS id_buku,
 FROM public.buku buku
 JOIN public.relasi_cabang_buku relasiCabangBuku ON relasiCabangBuku.id_buku = buku.id;
 
-COMMIT;
+CREATE VIEW public.buku_dengan_penulis AS
+SELECT buku.id AS id_buku,
+        buku.judul AS judul_buku,
+        buku.ISBN AS ISBN,
+        penulis.nama,
+		penulis.id AS id_penulis
+FROM public.relasi_penulis_buku relasiPenulisBuku
+JOIN public.buku buku ON relasiPenulisBuku.id_buku = buku.id
+JOIN public.penulis penulis ON relasiPenulisBuku.id_penulis = penulis.id;
 
-SELECT * FROM public.promo_buku;
-SELECT * FROM public.buku_dengan_cabang;
+COMMIT;
 END;
